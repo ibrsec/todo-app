0f5a617ec3b846832b86eb1c9aa3f9c9e47a3c97
ibr <ibr.seckin@gmail.com>
2024-02-26 08:25:01 +0300

    inital commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const input= document.querySelector(".new-task-input");
+const addBtn = document.querySelector(".add-task-form button");
+const itemContainer = document.querySelector(".tasks-list");
+const deleteBtn = document.querySelector(".delete-btn");
